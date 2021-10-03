42e7be66a508ecc8e4c0e3a96e91e3173f635951
MohammedKamle <66168802+MohammedKamle@users.noreply.github.com>
2021-10-03 12:04:43 +0530

    added PUT ampping

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
@@ -34,6 +36,25 @@ public class StudentService {
 		}
 		studentRepository.deleteById(studentId);
 
+	}
+	
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email) {
+		Student student = studentRepository.findById(studentId)
+				.orElseThrow(() -> new IllegalStateException("Student with id "+studentId+" does not exists"));
+		
+		if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+			student.setName(name);
+		}
+		
+		if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email) ) {
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+			if (studentOptional.isPresent()) {
+				throw new IllegalStateException("Email Taken");
+			}
+			
+			student.setEmail(email);
+		}
 	}
 
 }
